b04ae73c1fa16dc33337c8bf31e9cb074ca39c6c
Dave Estey <daveestey.working@gmail.com>
2022-11-29 19:40:05 -0400

    starting with the fetch function

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,1 +1,27 @@
 
+
+function searchApi(query, format) {
+  var locQueryUrl = "https://api.exchange.cryptomkt.com/api/3";
+
+  if (format) {
+    locQueryUrl = "https://api.exchange.cryptomkt.com/api/3" + format + "/?fo=json";
+  }
+
+  locQueryUrl = locQueryUrl + "&q=" + query;
+
+  fetch(locQueryUrl)
+    .then(function (response) {
+      if (!response.ok) {
+        response.json().then(function (data) {
+          console.log(data)
+        });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    })
+      .catch(function () {
+        alert('Unable to connect to GitHub');
+      })
+    }
+
+searchApi()
