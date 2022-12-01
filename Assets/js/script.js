c3d4a93333a9328191442963b61aa98262a0598d
Max <max.belenchuk@gmail.com>
2022-12-01 14:45:02 -0400

    Targeting tickers for specific user input and inputing that ticker into a text area.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,24 @@
 
 
+
+
+function showResults(resultObj) {
+  var result = document.createElement('p');
+  result.innerHTML = resultObj.results[1].ticker; 
+  var inputData = document.querySelector("#textResult");
+  inputData.append(result);
+
+
+
+
+}
+
+
+
+
+
 function cryptoApi() {
+  console.log(coinGeckoApi);
   var coinGeckoApi = "https://api.coingecko.com/api/v3/coins/categories/list";
 
   fetch(coinGeckoApi)
@@ -21,14 +39,17 @@ function cryptoApi() {
 cryptoApi();
 
 function stockApi() {
-  var coinPolygonApi = "https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2020-06-01/2020-06-17?apiKey=UAmJhIVKMGMQmJfv7Tja6hKiWkViJV6z";
-  console.log (coinPolygonApi)
-  fetch(coinPolygonApi)
+  var coinPolygonReference = "https://api.polygon.io/v3/reference/tickers?market=stocks&active=true&apiKey=UAmJhIVKMGMQmJfv7Tja6hKiWkViJV6z";
+  var input = "Electronic" 
+  coinPolygonReference = ("https://api.polygon.io/v3/reference/tickers?market=stocks&active=true&search="+input+"&apiKey=UAmJhIVKMGMQmJfv7Tja6hKiWkViJV6z")
+  console.log (coinPolygonReference)
+  fetch(coinPolygonReference)
     .then(function (response) {
       console.log(response)
       if (response.ok) {
         response.json().then(function (data) {
           console.log(data)
+          showResults(data);
         });
       } else {
         alert('Error: ' + response.statusText);
@@ -39,4 +60,7 @@ function stockApi() {
       })
     }
 
+
+
 stockApi();
+
