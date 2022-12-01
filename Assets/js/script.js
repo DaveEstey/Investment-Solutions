e5927f965beac40ea24d014e7f74185bfd44c064
Max <max.belenchuk@gmail.com>
2022-12-01 12:46:50 -0400

    Called another API for stock market ticker price, the call function works.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,9 +1,29 @@
 
 
-function searchApi() {
-  var locQueryUrl = "https://api.coingecko.com/api/v3/coins/categories/list";
+function cryptoApi() {
+  var coinGeckoApi = "https://api.coingecko.com/api/v3/coins/categories/list";
 
-  fetch(locQueryUrl)
+  fetch(coinGeckoApi)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          console.log(data)
+        });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    })
+      .catch(function () {
+        alert('Unable to connect to GitHub');
+      })
+    }
+
+cryptoApi();
+
+function stockApi() {
+  var coinPolygonApi = "https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2020-06-01/2020-06-17?apiKey=UAmJhIVKMGMQmJfv7Tja6hKiWkViJV6z";
+  console.log (coinPolygonApi)
+  fetch(coinPolygonApi)
     .then(function (response) {
       console.log(response)
       if (response.ok) {
@@ -19,4 +39,4 @@ function searchApi() {
       })
     }
 
-searchApi();
+stockApi();
