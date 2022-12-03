56845031ac1ecb10a8ff2683666e7b5d6fc63121
Ren Besson <ren.besson@outlook.com>
2022-12-03 12:00:14 -0400

    Search column 50%

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -12,7 +12,7 @@ function showStockResults(resultObj) {
     resultObj
   )}</textarea>`;
 
-/*   for (var i = 0; i < resultObj.results.length; i++) {
+  /*   for (var i = 0; i < resultObj.results.length; i++) {
     var result = document.createElement("div");
     result.innerHTML = resultObj.results[i].ticker;
 
@@ -29,22 +29,20 @@ function showCryptoResults(resultObj) {
 
     inputData.appendChild(result);
   }
-
-
-
-
-
 }
 
 function cryptoApi() {
-  var input = "music"
-  var coinGeckoApi = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&category=" + input + "&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+  var input = "music";
+  var coinGeckoApi =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&category=" +
+    input +
+    "&order=market_cap_desc&per_page=10&page=1&sparkline=false";
 
   fetch(coinGeckoApi)
     .then(function (response) {
       if (response.ok) {
         response.json().then(function (data) {
-          console.log(data)
+          console.log(data);
           showCryptoResults(data);
         });
       } else {
@@ -52,12 +50,11 @@ function cryptoApi() {
       }
     })
     .catch(function () {
-      alert('Unable to connect to GitHub');
-    })
+      alert("Unable to connect to GitHub");
+    });
 }
 
 // cryptoApi();
-
 
 function stockApi() {
   var coinPolygonReference =
@@ -85,10 +82,29 @@ function stockApi() {
     });
 }
 
+function getCryptoApi(value) {
+  var coinGeckoApi = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&category=${value}&order=market_cap_desc&per_page=10&page=1&sparkline=false`;
+
+  fetch(coinGeckoApi)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          console.log(data);
+          showStockResults(data);
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function () {
+      alert("Unable to connect to GitHub");
+    });
+}
+
 ///////// Query "value" from stock API /////////
 function getStockApi(value) {
   coinPolygonReference = `https://api.polygon.io/v3/reference/tickers?market=stocks&active=true&search=${value}&apiKey=UAmJhIVKMGMQmJfv7Tja6hKiWkViJV6z`;
-  console.log(coinPolygonReference);
+
   fetch(coinPolygonReference)
     .then(function (response) {
       if (response.ok) {
@@ -112,22 +128,19 @@ var searchInputEl = document.getElementById("searchInput");
 ///////// Add the Search button event listener /////////
 searchEl.addEventListener("click", () => {
   if (toggleEl.checked) getStockApi(searchInputEl.value);
+  if (!toggleEl.checked) getCryptoApi(searchInputEl.value);
 });
 
 // stockApi();
 
 var hideValueOptions = document.querySelector("#format-input-category");
-var hideValueCategory = document.querySelector('#format-input-type'); 
+var hideValueCategory = document.querySelector("#format-input-type");
 
 valueOptions.style.visibility = "hidden";
 
-
-hideValueCategory.addEventListener('change', function (event) {
- if (event.target.value == "stocks") {
-  hideValueOptions.removeAttribute("style");
- }
-
-else {
-  
-}
-})
+hideValueCategory.addEventListener("change", function (event) {
+  if (event.target.value == "stocks") {
+    hideValueOptions.removeAttribute("style");
+  } else {
+  }
+});
