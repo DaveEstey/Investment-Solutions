a058c13eccdebbea3674ed7db084c7a11012904b
Ren Besson <ren.besson@outlook.com>
2022-12-06 02:38:53 -0400

    Modal Implemented

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,12 +1,23 @@
+///////// Defines the HTML elements /////////
+var toggleEl = document.getElementById("cryptoStockToggle");
+var dropDownEl = document.getElementById("dropDownCategory");
 var contentEl = document.getElementById("content");
+var modalTitle = document.getElementById("modal-title");
+var modalText = document.getElementById("modal-text");
 
 function showStockResults(resultObj) {
-  /*   contentEl.innerHTML = `<textarea class="textarea h-full w-full" placeholder="Bio">${JSON.stringify(
-    resultObj
-  )}</textarea>`; */
-  printCards(resultObj);
+  clearDropDown();
+  resultObj.forEach((element) => {
+    var result = document.createElement("option");
+    result.innerHTML = element.description;
+    result.setAttribute("value", element.code);
+
+    dropDownEl.appendChild(result);
+  });
 }
+
 function showCryptoResults(resultObj) {
+  clearDropDown();
   for (var i = 0; i < resultObj.length; i++) {
     var result = document.createElement("option");
     result.innerHTML = resultObj[i].name;
@@ -16,8 +27,24 @@ function showCryptoResults(resultObj) {
   }
 }
 
-var printCards = (data) => {
+function clearContent() {
   contentEl.innerHTML = "";
+}
+
+function clearDropDown() {
+  dropDownEl.innerHTML = `<option disabled selected>
+      Please select an option...
+    </option>`;
+}
+
+function toggleModal() {
+  var mainModal = document.getElementById("main-modal");
+
+  if (mainModal.checked) !mainModal.checked;
+}
+
+var printCryptoCards = (data) => {
+  clearContent();
   data.forEach((element) => {
     var card = document.createElement("div");
     card.innerHTML = `<div class="card w-64 bg-white shadow-xl m-5">
@@ -33,6 +60,52 @@ var printCards = (data) => {
   });
 };
 
+var printStockCards = (data) => {
+  clearContent();
+  data.forEach((element) => {
+    var card = document.createElement("div");
+    card.innerHTML = `<div class="card w-64 bg-white shadow-xl m-5">
+    <div class="card-body">
+      <h3 class="card-title">${element.name}</h3>
+      <p>Ticker: ${element.ticker}</p>
+      <div class="card-actions justify-end">  
+        <label for="main-modal" class="btn btn-primary" id=${element.ticker}>open modal</label>
+      </div>
+    </div>
+  </div>`;
+    contentEl.append(card);
+  });
+};
+
+function getStockNews(ticker) {
+  var getNewsApi = `https://api.polygon.io/v2/reference/news?ticker=${ticker}&apiKey=UAmJhIVKMGMQmJfv7Tja6hKiWkViJV6z`;
+
+  fetch(getNewsApi)
+    .then(function (response) {
+      modalTitle.innerHTML = "";
+      modalText.innerHTML = "";
+      if (response.ok) {
+        response.json().then(function (data) {
+          console.log(data);
+
+          if (data.results.length > 0) {
+            modalTitle.innerHTML = data.results[0].title;
+            modalText.innerHTML = data.results[0].description;
+          } else {
+            modalTitle.innerHTML = "No News Found";
+            modalText.innerHTML = "No news found for this specific ticker";
+          }
+        });
+      } else {
+        modalTitle.innerHTML = "Error fetching news";
+        modalText.innerHTML = `Code: ${response.status}`;
+      }
+    })
+    .catch(function () {
+      alert("Unable to connect to GitHub");
+    });
+}
+
 function getCryptoApi(value) {
   var coinGeckoApi = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&category=${value}&order=market_cap_desc&per_page=10&page=1&sparkline=false`;
 
@@ -41,12 +114,32 @@ function getCryptoApi(value) {
       if (response.ok) {
         response.json().then(function (data) {
           console.log(data);
-          printCards(data);
-          showStockResults(data);
-          makeOptions(data);
+          printCryptoCards(data);
         });
       } else {
-        alert("Error: " + response.statusText);
+        var modalTitle = document.getElementById("modal-title");
+        var modalText = document.getElementById("modal-text");
+      }
+    })
+    .catch(function () {
+      alert("Unable to connect to GitHub");
+    });
+}
+
+///////// Query "value" from stock API /////////
+function getStockApi(ticker) {
+  coinPolygonReference = `https://api.polygon.io/v3/reference/tickers?type=${ticker}&market=stocks&active=true&apiKey=UAmJhIVKMGMQmJfv7Tja6hKiWkViJV6z`;
+
+  fetch(coinPolygonReference)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          console.log(data);
+          printStockCards(data.results);
+        });
+      } else {
+        modalTitle.innerHTML = "Error fetching news";
+        modalText.innerHTML = `Code: ${response.status}`;
       }
     })
     .catch(function () {
@@ -61,9 +154,7 @@ function getCryptoCatagories() {
     .then(function (response) {
       if (response.ok) {
         response.json().then(function (data) {
-          console.log(data);
           showCryptoResults(data);
-          makeOptions(data);
         });
       } else {
         alert("Error: " + response.statusText);
@@ -75,15 +166,14 @@ function getCryptoCatagories() {
 }
 
 ///////// Query "value" from stock API /////////
-function getStockApi(value) {
-  coinPolygonReference = `https://api.polygon.io/v3/reference/tickers?market=stocks&active=true&search=${value}&apiKey=UAmJhIVKMGMQmJfv7Tja6hKiWkViJV6z`;
+function getStockCategories() {
+  polygonReference = `https://api.polygon.io/v3/reference/tickers/types?asset_class=stocks&locale=us&apiKey=UAmJhIVKMGMQmJfv7Tja6hKiWkViJV6z`;
 
-  fetch(coinPolygonReference)
+  fetch(polygonReference)
     .then(function (response) {
       if (response.ok) {
         response.json().then(function (data) {
-          showStockResults(data);
-          makeOptions(data);
+          showStockResults(data.results);
         });
       } else {
         alert("Error: " + response.statusText);
@@ -94,18 +184,24 @@ function getStockApi(value) {
     });
 }
 
-///////// Defines the HTML elements /////////
-var toggleEl = document.getElementById("cryptoStockToggle");
-var searchEl = document.getElementById("searchBtn");
-var dropDownEl = document.getElementById("dropDownCategory");
-
 ///////// Add the Search button event listener /////////
 dropDownEl.addEventListener("change", () => {
-  getCryptoApi(dropDownEl.value);
+  if (!toggleEl.checked) getCryptoApi(dropDownEl.value);
+  if (toggleEl.checked) getStockApi(dropDownEl.value);
 });
+
 getCryptoCatagories();
 
-function makeOptions(data) {
+toggleEl.addEventListener("change", (event) => {
+  isChecked = event.target.checked;
+
+  clearContent();
+
+  if (!isChecked) getCryptoCatagories();
+  else getStockCategories();
+});
+
+/* function makeOptions(data) {
   //needs STYLING AND CHANGE TO OTHER INPUT FEILD
   var makeOption = [];
   if (toggleEl.checked) {
@@ -123,7 +219,7 @@ function makeOptions(data) {
     });
   }
 }
-
+ */
 /* function makeCatagories(cataData){
  for (var i = 0; i < resultObj.results.length; i++) {
     var result = document.createElement("div");
@@ -135,4 +231,9 @@ function makeOptions(data) {
 
 document.querySelector("#resetBtn").addEventListener("click", () => {
   window.location.reload(true);
+});
+
+contentEl.addEventListener("click", (event) => {
+  console.log(event.target.id);
+  getStockNews(event.target.id);
 });
